63fdb9a8667724b695add16de15cfd4e5e91042a
piersonjackson <143756088+piersonjackson@users.noreply.github.com>
2023-08-31 17:05:41 -0400

    Add files via upload

---
+++ CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Jackson Pierson" << std::endl;
+	return 0;
+}
